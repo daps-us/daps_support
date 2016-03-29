899742e53fdc6015200e358d1937851d679f3c60
Raul Dominguez <radag87@yahoo.com>
2016-03-28 18:48:44 -0700

    added a daps.site.restore.sh and some drush commands that take the site on and off line as well as the latest htaccess file

---
+++ bin/drush.site.online.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# this script will be used to restore a site that.
+
+echo $0 started at `date`
+
+# source the site specific settings
+. ../cfg/site.cfg
+if [[ $? -ne 0 ]];then
+    echo "ERROR attempting to fetch site settings."
+    exit 1
+fi
+
+# sanity checks
+if [[ -e ${DOCROOT} ]]; then
+    if [[ ! -d ${DOCROOT} ]];then
+        echo "ERROR ${DOCROOT} is not a directory."
+        exit 1
+    fi
+fi
+
+# go to docroot
+cd ${DOCROOT}
+if [[ $? -ne 0 ]]
+then
+   echo "ERROR Unable to access ${DOCROOT}"
+   exit 1
+fi
+
+drush vset maintenance_mode 0
+if [[ $? -ne 0 ]]
+then
+   echo "ERROR taking site out of maintenance mode"
+   exit 1
+fi
+echo $0 finished at `date`
+exit 0
